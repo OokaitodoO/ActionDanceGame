f2bc46b2e8565eb264d316c15bca515ddb1f6559
mixer <m.kanjanawat@gmail.com>
2025-11-07 18:01:13 +0700

    [Add] Rhythm processor

---
+++ Assets/Scripts/RhythmProcessor.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Rhythm
+{
+    public class RhythmProcessor : MonoBehaviour
+    {
+        [SerializeField] private PlayableDirector;
+
+        private float currentTime;
+    }
+}
+
+
