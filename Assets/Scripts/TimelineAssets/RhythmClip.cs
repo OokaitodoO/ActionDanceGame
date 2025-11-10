af2529c4a40b70128582fb7c103be0b3b2a46b4a
Kanjanawat <m.kanjanawat@gmail.com>
2025-11-10 11:53:05 +0700

    [Mod] Hotfix

--- Assets/Scripts/TimelineAssets/RhythmClip.cs
+++ Assets/Scripts/TimelineAssets/RhythmClip.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 [System.Serializable]
 [CreateAssetMenu(fileName = "NewRhythmClip", menuName = "Timeline/RhythmClip")]
-public class RhythmClip : PlayableAsset
+public class RhythmClip : PlayableAsset, ITimelineClipAsset
 {
     [SerializeField] private GameObject prefab;
+    [Header("Settings")]
+    [SerializeField] private double spawnTime;
+
+    public ClipCaps clipCaps => throw new System.NotImplementedException();
 
     public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
     {
