eaddb17cf237ad0e1f2a88fa8d0131036afae920
mixer <m.kanjanawat@gmail.com>
2025-11-17 17:59:38 +0700

    [Add] Swipe note random rotation

--- Assets/Scripts/Notes/SwipeNoteController.cs
+++ Assets/Scripts/Notes/SwipeNoteController.cs
@@ -4,12 +4,26 @@ using UnityEngine.UI;
 
 public class SwipeNoteController : BaseNote
 {
-    [SerializeField] private Image directionSprite;
     [Space]
-    [SerializeField] private Sprite arrow;
+    [SerializeField] private Image directionSprite;
+    //[SerializeField] private Sprite arrow;
+
+    private float[] rotation =
+    {
+        0, //Down
+        90, //Right
+        180, // up
+        270 //Left
+    };
 
     [HideInInspector]
     public DirecitonConfig inputDirection;
+
+    public override void Initialize()
+    {
+        RandomDirection();
+        base.Initialize();
+    }
 
     public override void OnPointerDown(PointerEventData eventData)
     {
@@ -20,5 +34,10 @@ public class SwipeNoteController : BaseNote
     {
         //Get current pointer position
         //Calculate direction
+    }
+
+    private void RandomDirection()
+    {
+
     }
 }
