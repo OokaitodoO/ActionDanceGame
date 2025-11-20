cea65d6e716d0fa6159ee67b04a2ab1d35297845
Kanjanawat <m.kanjanawat@gmail.com>
2025-11-20 23:44:04 +0700

    [Add][Mod] ไม่ห่างกัน, Fix tempo generation

--- Assets/Scripts/TimelineAssets/TempoTrack.cs
+++ Assets/Scripts/TimelineAssets/TempoTrack.cs
@@ -5,11 +5,13 @@ using UnityEngine.Playables;
 using UnityEngine.Timeline;
 using System.Linq;
 using System.Collections.Generic;
+using System;
 
 [TrackClipType(typeof(TempoClip))]
 public class TempoTrack : TrackAsset
 {
     private const float FIX_DURATION = 0.025f;
+    private const float SONGLENGTH = 90f;
 
     public float BPM;
     public float offset;
@@ -23,7 +25,10 @@ public class TempoTrack : TrackAsset
 
         double beatInterval = 60.0 / BPM;
 
-        for (int i = 0; i < 90; i++)
+        //Find number of tempo in 90 sec
+        var amount = FindNumberOfBeat();
+
+        for (int i = 0; i < amount; i++)
         {
             double newStartTime = offset + (i * beatInterval);
             var newClip = CreateClip<TempoClip>();
@@ -44,5 +49,26 @@ public class TempoTrack : TrackAsset
                 DeleteClip(clip);
             }
         }
+    }
+
+    private int FindNumberOfBeat()
+    {
+        double beatInterval = 60.0 / BPM;
+
+        double availableTime = SONGLENGTH - offset;
+
+        if (availableTime <= 0)
+        {
+            Debug.LogWarning("Offset exceeds total duration limit. No beats will be generated.");
+            return 0;
+        }
+        
+        double rawBeatAmount = availableTime / beatInterval;
+        
+        int amount = (int)Math.Floor(rawBeatAmount);
+
+        //Debug.Log($"BPM: {BPM}, Beat Interval: {beatInterval:F3}s, Available Time: {availableTime:F3}s, Beats to generate: {amount}");
+
+        return amount;
     }
 }
